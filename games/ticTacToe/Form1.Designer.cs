e97c71334cac31ba3a94e43f7d7d676337637cba
CheeseTheCat <james.hooper@tooeleschools.org>
2021-11-01 10:12:12 -0600

    TicTacToe

---
+++ games/ticTacToe/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace ticTacToe
+{
+    partial class ticTacToeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.topLeftSquareBttn = new System.Windows.Forms.Button();
+            this.topMidBttn = new System.Windows.Forms.Button();
+            this.topRightBttn = new System.Windows.Forms.Button();
+            this.midLeftBttn = new System.Windows.Forms.Button();
+            this.midMidBttn = new System.Windows.Forms.Button();
+            this.midRightBttn = new System.Windows.Forms.Button();
+            this.botmMidBttn = new System.Windows.Forms.Button();
+            this.botmRightBttn = new System.Windows.Forms.Button();
+            this.turnLbl = new System.Windows.Forms.Label();
+            this.resetBttn = new System.Windows.Forms.Button();
+            this.xScoreLbl = new System.Windows.Forms.Label();
+            this.oScoreLable = new System.Windows.Forms.Label();
+            this.botmLeftBttn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // topLeftSquareBttn
+            // 
+            this.topLeftSquareBttn.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.topLeftSquareBttn.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topLeftSquareBttn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.topLeftSquareBttn.Location = new System.Drawing.Point(12, 12);
+            this.topLeftSquareBttn.Name = "topLeftSquareBttn";
+            this.topLeftSquareBttn.Size = new System.Drawing.Size(100, 100);
+            this.topLeftSquareBttn.TabIndex = 0;
+            this.topLeftSquareBttn.UseVisualStyleBackColor = false;
+            this.topLeftSquareBttn.Click += new System.EventHandler(this.topLeftSquareBttn_Click);
+            // 
+            // topMidBttn
+            // 
+            this.topMidBttn.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.topMidBttn.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topMidBttn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.topMidBttn.Location = new System.Drawing.Point(118, 12);
+            this.topMidBttn.Name = "topMidBttn";
+            this.topMidBttn.Size = new System.Drawing.Size(100, 100);
+            this.topMidBttn.TabIndex = 1;
+            this.topMidBttn.UseVisualStyleBackColor = false;
+            this.topMidBttn.Click += new System.EventHandler(this.topLeftSquareBttn_Click);
+            // 
+            // topRightBttn
+            // 
+            this.topRightBttn.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.topRightBttn.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topRightBttn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.topRightBttn.Location = new System.Drawing.Point(224, 12);
+            this.topRightBttn.Name = "topRightBttn";
+            this.topRightBttn.Size = new System.Drawing.Size(100, 100);
+            this.topRightBttn.TabIndex = 2;
+            this.topRightBttn.UseVisualStyleBackColor = false;
+            this.topRightBttn.Click += new System.EventHandler(this.topLeftSquareBttn_Click);
+            // 
+            // midLeftBttn
+            // 
+            this.midLeftBttn.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.midLeftBttn.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.midLeftBttn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.midLeftBttn.Location = new System.Drawing.Point(12, 118);
+            this.midLeftBttn.Name = "midLeftBttn";
+            this.midLeftBttn.Size = new System.Drawing.Size(100, 100);
+            this.midLeftBttn.TabIndex = 3;
+            this.midLeftBttn.UseVisualStyleBackColor = false;
+            this.midLeftBttn.Click += new System.EventHandler(this.topLeftSquareBttn_Click);
+            // 
+            // midMidBttn
+            // 
+            this.midMidBttn.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.midMidBttn.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.midMidBttn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.midMidBttn.Location = new System.Drawing.Point(118, 118);
+            this.midMidBttn.Name = "midMidBttn";
+            this.midMidBttn.Size = new System.Drawing.Size(100, 100);
+            this.midMidBttn.TabIndex = 4;
+            this.midMidBttn.UseVisualStyleBackColor = false;
+            this.midMidBttn.Click += new System.EventHandler(this.topLeftSquareBttn_Click);
+            // 
+            // midRightBttn
+            // 
+            this.midRightBttn.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.midRightBttn.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.midRightBttn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.midRightBttn.Location = new System.Drawing.Point(224, 118);
+            this.midRightBttn.Name = "midRightBttn";
+            this.midRightBttn.Size = new System.Drawing.Size(100, 100);
+            this.midRightBttn.TabIndex = 5;
+            this.midRightBttn.UseVisualStyleBackColor = false;
+            this.midRightBttn.Click += new System.EventHandler(this.topLeftSquareBttn_Click);
+            // 
+            // botmMidBttn
+            // 
+            this.botmMidBttn.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.botmMidBttn.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botmMidBttn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.botmMidBttn.Location = new System.Drawing.Point(118, 224);
+            this.botmMidBttn.Name = "botmMidBttn";
+            this.botmMidBttn.Size = new System.Drawing.Size(100, 100);
+            this.botmMidBttn.TabIndex = 7;
+            this.botmMidBttn.UseVisualStyleBackColor = false;
+            this.botmMidBttn.Click += new System.EventHandler(this.topLeftSquareBttn_Click);
+            // 
+            // botmRightBttn
+            // 
+            this.botmRightBttn.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.botmRightBttn.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botmRightBttn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.botmRightBttn.Location = new System.Drawing.Point(224, 224);
+            this.botmRightBttn.Name = "botmRightBttn";
+            this.botmRightBttn.Size = new System.Drawing.Size(100, 100);
+            this.botmRightBttn.TabIndex = 8;
+            this.botmRightBttn.UseVisualStyleBackColor = false;
+            this.botmRightBttn.Click += new System.EventHandler(this.topLeftSquareBttn_Click);
+            // 
+            // turnLbl
+            // 
+            this.turnLbl.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.turnLbl.Location = new System.Drawing.Point(327, 80);
+            this.turnLbl.Name = "turnLbl";
+            this.turnLbl.Size = new System.Drawing.Size(93, 32);
+            this.turnLbl.TabIndex = 9;
+            this.turnLbl.Text = "Turn: X";
+            this.turnLbl.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // resetBttn
+            // 
+            this.resetBttn.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.resetBttn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.resetBttn.Location = new System.Drawing.Point(330, 292);
+            this.resetBttn.Name = "resetBttn";
+            this.resetBttn.Size = new System.Drawing.Size(90, 32);
+            this.resetBttn.TabIndex = 10;
+            this.resetBttn.Text = "Reset";
+            this.resetBttn.UseVisualStyleBackColor = false;
+            this.resetBttn.Click += new System.EventHandler(this.resetBttn_Click);
+            // 
+            // xScoreLbl
+            // 
+            this.xScoreLbl.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.xScoreLbl.Location = new System.Drawing.Point(330, 12);
+            this.xScoreLbl.Name = "xScoreLbl";
+            this.xScoreLbl.Size = new System.Drawing.Size(90, 32);
+            this.xScoreLbl.TabIndex = 11;
+            this.xScoreLbl.Text = "X =  ";
+            this.xScoreLbl.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // oScoreLable
+            // 
+            this.oScoreLable.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.oScoreLable.Location = new System.Drawing.Point(330, 46);
+            this.oScoreLable.Name = "oScoreLable";
+            this.oScoreLable.Size = new System.Drawing.Size(90, 32);
+            this.oScoreLable.TabIndex = 12;
+            this.oScoreLable.Text = "O =  ";
+            this.oScoreLable.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // botmLeftBttn
+            // 
+            this.botmLeftBttn.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.botmLeftBttn.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botmLeftBttn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.botmLeftBttn.Location = new System.Drawing.Point(12, 224);
+            this.botmLeftBttn.Name = "botmLeftBttn";
+            this.botmLeftBttn.Size = new System.Drawing.Size(100, 100);
+            this.botmLeftBttn.TabIndex = 13;
+            this.botmLeftBttn.UseVisualStyleBackColor = false;
+            this.botmLeftBttn.Click += new System.EventHandler(this.topLeftSquareBttn_Click);
+            // 
+            // ticTacToeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(434, 337);
+            this.Controls.Add(this.botmLeftBttn);
+            this.Controls.Add(this.oScoreLable);
+            this.Controls.Add(this.xScoreLbl);
+            this.Controls.Add(this.resetBttn);
+            this.Controls.Add(this.turnLbl);
+            this.Controls.Add(this.botmRightBttn);
+            this.Controls.Add(this.botmMidBttn);
+            this.Controls.Add(this.midRightBttn);
+            this.Controls.Add(this.midMidBttn);
+            this.Controls.Add(this.midLeftBttn);
+            this.Controls.Add(this.topRightBttn);
+            this.Controls.Add(this.topMidBttn);
+            this.Controls.Add(this.topLeftSquareBttn);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "ticTacToeForm";
+            this.Text = "TicTacToe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button topLeftSquareBttn;
+        private System.Windows.Forms.Button topMidBttn;
+        private System.Windows.Forms.Button topRightBttn;
+        private System.Windows.Forms.Button midLeftBttn;
+        private System.Windows.Forms.Button midMidBttn;
+        private System.Windows.Forms.Button midRightBttn;
+        private System.Windows.Forms.Button botmMidBttn;
+        private System.Windows.Forms.Button botmRightBttn;
+        private System.Windows.Forms.Label turnLbl;
+        private System.Windows.Forms.Button resetBttn;
+        private System.Windows.Forms.Label xScoreLbl;
+        private System.Windows.Forms.Label oScoreLable;
+        private System.Windows.Forms.Button botmLeftBttn;
+    }
+}
+
---
+++ games/ticTacToe/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ticTacToe
+{
+    public partial class ticTacToeForm : Form
+    {
+        public ticTacToeForm()
+        {
+            InitializeComponent();
+        }
+
+        public string XToken = "X";
+        public string OToken = "O";
+        public string turn = "X";
+        public int xScore = 0;
+        public int oScore = 0;
+        private void topLeftSquareBttn_Click(object sender, EventArgs e)
+        {
+            // set the text of the button to the turn's token
+            Button button = (Button) sender;
+            if (button.Text == "")
+            {
+                button.Text = turn;
+                
+            }
+            else
+            {
+                return;
+            }
+
+            // Check for win
+            // Switch the turn
+            switchTurn();
+            // Update Text
+            update_message();
+            // Disable button so it can't be clicked
+
+        }
+
+        private string checkForWin()
+        {
+            // Cheese for a win and return the winning token
+            string winner = XToken;
+
+            return winner;
+        }
+
+        private void switchTurn()
+        {
+            // switch our turns
+
+            if (turn != XToken)
+            {
+                turn = XToken;
+            }
+            else
+            {
+                turn = OToken;
+            }
+
+        }
+
+        private void update_scores()
+        {
+            // update the player scores
+        }
+
+        private void update_message()
+        {
+            // Update the hud message
+            turnLbl.Text = "Turn: " + turn;
+        }
+
+        private void resetBttn_Click(object sender, EventArgs e)
+        {
+            // Clear our board
+            turn = XToken;
+            update_message();
+            topLeftSquareBttn.Text = "";
+            topMidBttn.Text = "";
+            topRightBttn.Text = "";
+            midLeftBttn.Text = "";
+            midMidBttn.Text = "";
+            midRightBttn.Text = "";
+            botmLeftBttn.Text = "";
+            botmMidBttn.Text = "";
+            botmRightBttn.Text = "";
+        }
+    }
+}
