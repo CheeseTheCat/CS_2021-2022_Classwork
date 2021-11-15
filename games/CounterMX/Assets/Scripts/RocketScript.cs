3f80c044be7f69a4b6d0b3ac0c2d3cae8e8b7fc8
CheeseTheCat <james.hooper@tooeleschools.org>
2021-11-15 10:15:38 -0700

    update

--- games/CounterMX/Assets/Scripts/RocketScript.cs
+++ games/CounterMX/Assets/Scripts/RocketScript.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class RocketScript : MonoBehaviour
 {
+
+
+    [SerializeField]
+    private AudioClip[] audioClips;
+
+    [Header("Components")]
+    [SerializeField]
+    private Rigidbody2D rig;
+    [SerializeField]
+    private AudioSource audioSrc;
+
+
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody2D> ();
+        audioSrc = GetComponent<AudioSource>();
+        audioSrc.PlayOneShot(audioClips[0]);
+        Destroy(this.gameObject, 3.5f);
+    }
+
+
+    public void launch(Vector2 direction, float speed)
+    {
+        rig.AddForce(direction.normalized * speed, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        audioSrc.PlayOneShot(audioClips[1]);
+        Destroy(this.gameObject, audioClips[1].length);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        launch( new Vector2(0, 0), 5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.forward * 5f);
     }
 }
